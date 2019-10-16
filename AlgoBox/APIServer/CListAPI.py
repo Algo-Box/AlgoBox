41d390bfaeeadabf2cad30c88b1cd34ba142bda7
Somesh Awasthi <21171.somesh@gmail.com>
2019-10-16 10:27:36 +0530

    Changed API fetching mechanism

--- AlgoBox/APIServer/CListAPI.py
+++ AlgoBox/APIServer/CListAPI.py
@@ -11,20 +11,20 @@ def getContestList():
 	baseaddress = "https://clist.by/api/v1/contest/?format=json&username="+API_UNAME+"&api_key="+API_KEY+"&order_by=start"
 	baseaddress += "&end__gte="
 	baseaddress += today
-	baseaddress += "&resource__name__in=codeforces.com,"
 
 	dict = {"objects":[]}
-	sites = {0:"codeforces.com", 1:"topcoder.com", 7:"hackerrank.com", 3:" facebook.com/hackercup", 4:"codingcompetitions.withgoogle.com", 5:"codeforces.com/gyms", 6:"hackerearth.com", 2:"codechef.com", 8:"open.kattis.com", 9:"csacademy.com", 10:"leetcode.com", 11:"kaggle.com", 12:"atcoder.jp"}
-	for i in range(1, len(sites)) :
-		baseaddress += ","
-		baseaddress += sites[i]
-	baseaddress += "&order_by=start"
-
-	data = requests.get(baseaddress)
-	pymap = json.loads(data.content)
-	for i in range(len(pymap["objects"])) :
-		dict["objects"].append(pymap["objects"][i])
+	sites = {0:"codeforces.com", 1:"topcoder.com", 2:"codechef.com", 3:" facebook.com/hackercup", 4:"codingcompetitions.withgoogle.com", 5:"codeforces.com/gyms", 6:"hackerearth.com", 7:"hackerrank.com", 8:"open.kattis.com", 9:"csacademy.com", 10:"leetcode.com", 11:"kaggle.com", 12:"atcoder.jp"}
+	limits = {0:3, 1:3, 2:4, 3:2, 4:2, 5:2, 6:10, 7:3, 8:3, 9:2, 10:3, 11:4, 12:4}
+	def getdata(i) :
+		adrs = baseaddress+"&resource__name="+sites[i]+"&limit="+str(limits[i])
+		data = requests.get(adrs)
+		pymap = json.loads(data.content)
+		for j in range(len(pymap["objects"])) :
+			dict["objects"].append(pymap["objects"][j])
+	for i in range(0, len(sites)) :
+		getdata(i)
 	jsonobject = json.dumps(dict)
+
 	return jsonobject
 
 def viewObject():
