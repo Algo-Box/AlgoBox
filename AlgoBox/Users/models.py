c90340a271ce5676a6a8e17587e2d43d7f470645
Shubham Sharma <shan61916@gmail.com>
2019-10-30 01:25:46 +0530

    extended user model

--- AlgoBox/Users/models.py
+++ AlgoBox/Users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.db.models import CharField
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    bio = models.TextField(max_length = 500, blank = True)
+    cc_handle = CharField(max_length = 40, blank = True)
+    cf_handle = CharField(max_length=40, blank = True)
+    sp_handle = CharField(max_length=40, blank = True)
+    ac_handle = CharField(max_length=40, blank = True)
+
+@receiver(post_save, sender = User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user = instance)
+    instance.profile.save()
+
+@receiver(post_save, sender = User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
